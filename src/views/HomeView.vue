bd53601af04a1059975b1bd627dd240cb79d4e64
MJ <126165004+squarePotatoe@users.noreply.github.com>
2025-02-13 09:27:47 +0000

    home view update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <main class="flex-1 p-4">
-      <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mb-6">
+    <main class="container relative justify-between h-full max-w-6xl px-10 mx-auto xl:px-0 mt-5">
+      <div class="flex flex-col mt-6 lg:flex-row gap-4 mb-6">
         <div
           class="flex-1 bg-indigo-100 border border-indigo-200 rounded-xl p-6 animate-fade-in"
         >
@@ -33,6 +33,19 @@
       <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         <RouterLink to="/classes" class="home-card animate-slide-up">
           <h3 class="text-xl font-bold text-indigo-800">My Classes</h3>
+          <hr class="text-gray-300 my-2" />
+          <div class="flex flex-col justify-center gap-2 mt-2">
+            <div>
+
+              <p class="text-lg font-semibold text-gray-600">
+                Stay updated with your schedule
+              </p>
+              <p class="text-sm text-gray-600">Next class: Space Travel</p>
+              <p class="text-sm text-gray-600">Teacher: John Doe</p>
+              <p class="text-sm text-gray-600">Date: 22 October 2025</p>
+              <p class="text-sm text-gray-600">Time: 19:00 - 20:00</p>
+            </div>
+          </div>
         </RouterLink>
         <RouterLink
           to="/writing"
@@ -40,6 +53,24 @@
           style="animation-delay: 0.2s"
         >
           <h3 class="text-xl font-bold text-indigo-800">Next lesson</h3>
+          <hr class="text-gray-300 my-2" />
+          <div class="flex flex-col justify-center gap-2 mt-2">
+            <img
+              src="@/assets/skysurfing.png"
+              alt="Skysurfing"
+              class="w-12 h-12 rounded-full"
+            />
+            <div>
+              <h2 class="text-lg font-semibold text-purple-700">
+                Article writing
+              </h2>
+              <p class="text-lg font-semibold text-gray-600">
+                The Evolution of Skysurfing
+              </p>
+              <p class="text-sm text-gray-600">John Doe</p>
+            </div>
+
+          </div>
         </RouterLink>
         <RouterLink
           to="/classes"
@@ -65,22 +96,23 @@
         <div
           class="flex-1 bg-blue-100 border border-blue-200 rounded-xl p-6 animate-fade-in"
         >
-          <!-- <h2 class="text-4xl md:text-5xl text-blue-900">
-                    Inbox <br><strong>23</strong>
+          <h2 class="text-4xl md:text-5xl text-blue-900">
+                    <br><strong></strong>
                 </h2>
                 <a href="#" class="inline-block mt-8 px-8 py-2 rounded-full text-xl font-bold text-white bg-blue-800 hover:bg-blue-900 transition-transform duration-300 hover:scale-105">
-                    See messages
-                </a> -->
+                    Coming soon
+                </a>
         </div>
       </div>
     </main>
-    <Features />
+    <!-- <Features /> -->
   </div>
 </template>
 
 <script setup>
 import { ref, computed, watch, onMounted, onUnmounted } from 'vue'
 import Features from '@/components/Features.vue'
+import ClassCard from './classes/components/ClassCard.vue'
 
 const currentTime = ref(new Date())
 
