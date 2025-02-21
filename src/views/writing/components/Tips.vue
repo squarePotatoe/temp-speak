36beb1dcb919614d98295f8db45c2437b6f6f8ee
squarePotatoe <mjdeoscode@outlook.com>
2025-02-22 00:00:15 +0800

    tips restyle

--- src/views/writing/components/Tips.vue
+++ src/views/writing/components/Tips.vue
@@ -2,13 +2,10 @@
   <div>
     <div class="h-[calc(100vh-100px)] overflow-auto">
       <h2 class="text-lg font-semibold pb-2">How to structure an {{ guide.textType }}</h2>
-      <p class="text-gray-600 pb-4">Follow these steps for the basic structure</p>
-      <div v-for="(item, index) in guide.structure" :key="index" class="flex flex-col overflow-y-auto shadow-md mb-4">
-        <div class="bg-yellow-50 p-4 rounded-md">
-          <h3 class="font-bold">{{ item.header }}</h3>
-          <p>{{ item.content }}</p>
-          <p class="italic text-gray-600">Example: {{ item.example }}</p>
-        </div>
+      <p class="text-gray-600 ">Follow these steps for the basic structure</p>
+      <p class="italic text-gray-500 pb-4">Mark the steps as you complete them</p>
+      <div v-for="(item, index) in guide.structure" :key="index" class="flex flex-col overflow-y-auto shadow-md mb-4" >
+        <Tip :header="item.header" :content="item.content" :example="item.example" />
       </div>
     </div>
   </div>
@@ -17,8 +14,11 @@
 <script setup>
 import { ref } from 'vue'
 import { guidesForTextTypes } from '@/data'
+import Tip from './Tip.vue'
 
 const guide = guidesForTextTypes.find(item => item.id === 1)
+
+const isDone = ref(false)
 </script>
 
 <style scoped>
