0110203003c1802c26ac3ee8387e63cf7e370633
MJ <126165004+squarePotatoe@users.noreply.github.com>
2025-06-18 19:38:33 +0800

    teacher dash preview button moved

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -91,18 +91,6 @@
         class="flex items-center text-gray-600 hover:text-indigo-800 py-4 transition-all duration-300 hover:translate-x-1"
       >
         <RouterLink
-          to="/teacherdash"
-          class="text-gray-600 hover:text-indigo-800 flex w-full"
-          @click="isOpen = !isOpen"
-        >
-          Teacher
-          <span class="material-icons-outlined ml-auto">keyboard_arrow_</span>
-        </RouterLink>
-      </div>
-      <div
-        class="flex items-center text-gray-600 hover:text-indigo-800 py-4 transition-all duration-300 hover:translate-x-1"
-      >
-        <RouterLink
           to="/"
           class="text-gray-600 hover:text-indigo-800 flex w-full"
           @click="isOpen = !isOpen"
@@ -114,6 +102,18 @@
           >
         </RouterLink>
       </div>
+    </div>
+    <div
+      class="flex items-center text-gray-600 hover:text-indigo-800 py-4 transition-all duration-300 hover:translate-x-1"
+    >
+      <RouterLink
+        to="/teacherdash"
+        class="text-gray-600 hover:text-indigo-800 flex w-full"
+        @click="isOpen = !isOpen"
+      >
+        Teacher dash preview
+        <span class="material-icons-outlined ml-auto">dashboard</span>
+      </RouterLink>
     </div>
   </aside>
 </template>
